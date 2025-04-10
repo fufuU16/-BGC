c13e2211cff8258fb5fa14fefa8159a8a2c53095
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2025-04-10 16:22:46 +0000

    test

--- CommandCenter/index.php
+++ CommandCenter/index.php
@@ -180,7 +180,6 @@ $conn->close();
         <label for="password">Password</label>
         <div class="password-container">
             <input type="password" id="password" name="password" placeholder="Enter Password" required>
-            <button type="button" class="toggle-password" onclick="togglePassword()">Show</button>
         </div>
         <div class="forgot-password">
             <a href="forgot_password.php">Forgot Password?</a>
